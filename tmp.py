a7b2c90eca2cde16e6420f99c0e8f837db1be0bb
godontop <tosony@126.com>
2018-01-08 23:54:32 +0800

    regular update

--- tmp.py
+++ tmp.py
@@ -1,7 +1,6 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-import io
-
-
-print(io.IOBase.__class__.__doc__)
+age = float(raw_input("Enter your age: "))
+grade = float(raw_input("Enter your grade: "))
+if age >= 8 and grade >= 3:
+    print "You can play this game."
+else:
+    print "You can't play this game."
