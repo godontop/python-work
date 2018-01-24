24945b7027e7df8445b35a5c01ba40ea3fdfa00f
godontop <tosony@126.com>
2018-01-24 22:14:37 +0800

    20180124

--- tmp.py
+++ tmp.py
@@ -1,33 +1,4 @@
 # coding=utf-8
 
-import random
-import sys
-
-import pygame
-
-from pygame.color import THECOLORS
-
-
-pygame.init()
-screen = pygame.display.set_mode([640, 480])
-screen.fill([255, 255, 255])
-for i in range(0, 100):
-    width = random.randint(0, 250)
-    height = random.randint(0, 100)
-    top = random.randint(0, 400)
-    left = random.randint(0, 500)
-    color_name = random.choice(THECOLORS.keys())
-    color = THECOLORS[color_name]
-    line_width = random.randint(1, 3)
-    pygame.draw.rect(screen, color, [left, top, width, height], line_width)
-
-
-running = True
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-    pygame.display.flip()
-    pygame.time.delay(30)
-
-pygame.quit()
+print(1, end=' ')
+print(2)
