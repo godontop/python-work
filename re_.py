cec0ee78a774517b7cc428e7e5e4d65e6b3e721b
godontop <tosony@126.com>
2018-03-01 15:22:13 +0800

    rename re.py as re_.py

---
+++ re_.py
@@ -0,0 +1,21 @@
+import re
+
+
+pattern = r"spam"
+
+if re.match(pattern, "spamspamspam"):
+    print("Match")
+else:
+    print("No match")
+
+if re.match(pattern, "eggspamsausagespam"):
+    print("Match")
+else:
+    print("No match")
+
+if re.search(pattern, "eggspamsausagespam"):
+    print("Match")
+else:
+    print("No match")
+
+print(re.findall(pattern, "eggspamsausagespam"))
