451819665226fc7664797e33883b3c18164e7ab3
godontop <tosony@126.com>
2018-03-07 17:54:17 +0800

    regular upadte

--- tmp.py
+++ tmp.py
@@ -1,17 +1,4 @@
 # coding=utf-8
 # Python 3 source code
 
-from urllib.request import urlopen, Request
-from bs4 import BeautifulSoup
-
-url = 'https://www.whatismybrowser.com/'
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.13; rv:58.0) \
-Gecko/20100101 Firefox/58.0'
-}
-
-req = Request(url)
-page = urlopen(req).read()
-soup = BeautifulSoup(page, 'lxml')
-myuseragent = soup.find_all('div', class_="user-agent")[0].a.get_text()
-print(myuseragent)
+import
