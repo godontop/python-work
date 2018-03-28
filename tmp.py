248cf28468e4b9d01698b2c4d07ddff916e8718c
godontop <tosony@126.com>
2018-03-29 07:18:25 +0800

    add test.txt file

--- tmp.py
+++ tmp.py
@@ -1,4 +1,26 @@
 # coding=utf-8
 # Python 3 source code
 
-import
+import re
+
+
+def link_crawler(seed_url, link_regex):
+    """Crawl from the given seed URL following links matched by link_regex
+    """
+    crawl_quene = [seed_url]
+    while crawl_quene:
+        url = crawl_quene.pop()
+        html = download(url)
+        # filter for links matching our regular expression
+        for link in get_links(html):
+            if re.match(link_regex, link):
+                crawl_quene.append(link)
+
+
+def get_links(html):
+    """Return a list of links from html
+    """
+    # a regular expression to extract all links from the webpage
+    webpage_regex = re.compile('<a[^>]+href=["\'](.*?)["\']', re.IGNORECASE)
+    # list of all links from the webpage
+    return webpage_regex.findall(html)
