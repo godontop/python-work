43b112d628c04f83f760ffd3803a00742d386929
godontop <tosony@126.com>
2018-05-24 08:16:55 +0800

    regular update

--- tmp.py
+++ tmp.py
@@ -1,11 +1,23 @@
-# coding=utf-8
-# Python 3 source code
+import urllib.request
+import urllib.parse
+import urllib.error
 
 
-def count(start=0, step=1):
-    # count(10) --> 10 11 12 13 14 ...
-    # count(2.5, 0.5) -> 2.5 3.0 3.5 ...
-    n = start
-    while True:
-        yield n
-        n += step
+def download(url, user_agent='wswp', proxy=None, num_retries=2):
+    print('Downloading:', url)
+    headers = {'User-agent': user_agent}
+    request = urllib.request.Request(url, headers=headers)
+    opener = urllib.request.build_opener()
+    if proxy:
+        proxy_params = {urllib.parse.urlparse(url).shceme: proxy}
+        opener.add_handler(urllib.request.ProxyHandler(proxy_params))
+    try:
+        html = opener.open(request).read()
+    except urllib.error.URLError as e:
+        print('Download error:', e.reason)
+        html = None
+        if num_retries > 0:
+            if hasattr(e, 'code') and 500 <= e.code < 600:
+                # retry 5XX HTTP errors
+                html = download(url, user_agent, proxy, num_retries - 1)
+    return html
