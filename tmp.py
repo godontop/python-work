56da1eb7c095219482c19eaa2020d578c8256265
godontop <tosony@126.com>
2018-05-28 08:29:33 +0800

    class Throttle

--- tmp.py
+++ tmp.py
@@ -1,23 +1,27 @@
-import urllib.request
+import datetime
+import time
 import urllib.parse
-import urllib.error
 
 
-def download(url, user_agent='wswp', proxy=None, num_retries=2):
-    print('Downloading:', url)
-    headers = {'User-agent': user_agent}
-    request = urllib.request.Request(url, headers=headers)
-    opener = urllib.request.build_opener()
-    if proxy:
-        proxy_params = {urllib.parse.urlparse(url).shceme: proxy}
-        opener.add_handler(urllib.request.ProxyHandler(proxy_params))
-    try:
-        html = opener.open(request).read()
-    except urllib.error.URLError as e:
-        print('Download error:', e.reason)
-        html = None
-        if num_retries > 0:
-            if hasattr(e, 'code') and 500 <= e.code < 600:
-                # retry 5XX HTTP errors
-                html = download(url, user_agent, proxy, num_retries - 1)
-    return html
+class Throttle:
+    """Add a delay between downloads to the same domain
+    """
+
+    def __init__(self, delay):
+        # amount of delay between downloads for each domain
+        self.delay = delay
+        # timestamp of when a domain was last accessed
+        self.domains = {}
+
+    def wait(self, url):
+        domain = urllib.parse.urlparse(url).netloc
+        last_accessed = self.domains.get(domain)
+        if self.delay > 0 and last_accessed is not None:
+            sleep_secs = self.delay - \
+                (datetime.datetime.now() - last_accessed).second
+            if sleep_secs > 0:
+                # domain has been accessed recently
+                # so need to sleep
+                time.sleep(sleep_secs)
+        # update the last accessed time
+        self.domains[domain] = datetime.datetime.now()
